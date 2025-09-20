e8bf11efb364cbc900875973a185373dd53b9ecd
DasMoge124 <kevindu0706@gmail.com>
2025-09-19 23:20:05 -0400

    Thanks to Jennie
    Co-authored-by: Jennie <ollime@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./pages/login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        <header class="nav_header">
+          <nav>
+            <ul class="nav_links">
+              <li class="nav_list">
+                <a class="nav_a" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_a" href="/Login">
+                  Login
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_a" href="learn.html">
+                  Learn
+                </a>
+              </li>
+              <li class="nav_list">
+                <a class="nav_a" href="/chessfull/connect/">
+                  Connect
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,22 @@
+export default Login;
+
+function Login() {
+  return (
+    <>
+      <div class="CONTAINER">
+        <div class="CARD">
+          <h3>Login</h3>
+          <input id="signInEmailInput" class="input" placeholder="Email" />
+          <input
+            id="signInPasswordInput"
+            class="input"
+            placeholder="Password"
+          />
+          <button class="signInButton" onclick="login_user()">
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
