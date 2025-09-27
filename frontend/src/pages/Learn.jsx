a4ab57d62419fa3a596e28eeee626c360fd0b98d
sahan pydah <146479899+sahanpydah@users.noreply.github.com>
2025-09-26 20:46:20 -0400

    Update Learn.jsx

--- frontend/src/pages/Learn.jsx
+++ frontend/src/pages/Learn.jsx
@@ -1,17 +1,18 @@
-export default Learn;
+
 // defines the learn page. This will be exported to App.jsx and rendered when the user navigates to /learn
 function Learn() { // learn component
 return(
-    <> 
-    <div class = "ButtonElements">
+    <>  
+    <div class = "Button">
         <button>Beginner</button>
         </div>
-    <div class = "ButtonElements">
+    <div class = "Button">
         <button>Intermediate</button>
         </div>
-    <div class = "ButtonElements">
+    <div class = "Button">
         <button>Advanced</button>
         </div>
     </>
 )
 }
+export default Learn;
