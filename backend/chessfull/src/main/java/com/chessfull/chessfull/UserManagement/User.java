9a68f11d147e15eaa9c15046e491ecb6afb1f14d
DasMoge124 <kevindu0706@gmail.com>
2025-11-01 16:41:13 -0400

    DATABASE DONE!

---
+++ backend/chessfull/src/main/java/com/chessfull/chessfull/UserManagement/User.java
@@ -0,0 +1,41 @@
+package com.chessfull.chessfull.UserManagement;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+    @Id
+    private String userId;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        super();
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
