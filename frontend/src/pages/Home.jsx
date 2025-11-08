4829bfe303634e87725882a8d6946ff6a7438f81
Nishant Y <nishantry3108@gmail.com>
2025-11-07 21:42:40 -0500

    home page css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,13 +5,19 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home-page-container">
+      {/* Decorative neon chess pieces */}
+      <div className="chess-decoration"></div>
+      <div className="chess-decoration"></div>
+      <div className="chess-decoration"></div>
+      <div className="chess-decoration"></div>
+      <div className="chess-decoration"></div>
+
       <div className="page-content">
         <h1>Welcome to Chessfull</h1>
         <p>Your journey to mastering chess starts here!</p>
         <div className="image-container">
           <img src={chessImage} alt="Chess players" />
         </div>
-        
       </div>
     </div>
   );
