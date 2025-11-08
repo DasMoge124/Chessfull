11d79bc343a5f161c86cca2f05974325642e7a79
DasMoge124 <kevindu0706@gmail.com>
2025-11-07 20:18:27 -0500

    CHANGES BRUH

--- frontend/src/pages/Beginner.jsx
+++ frontend/src/pages/Beginner.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
 import MagnusVSina from "./lessons/black/magnus_v_sina";
+import Forks from "./lessons/beginner_lessons/forks";
 
 function Beginner() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Beginner Page</h1>;
-      <MagnusVSina />;
+      <div className="ButtonElements">
+        <button onClick={() => navigate("/lessons/beginner/forks")}>
+          Forks
+        </button>
+      </div>
     </div>
   );
 }
