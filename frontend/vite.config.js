77f031ff123b8c4f7d7a78986f66fd2650bee806
DasMoge124 <kevindu0706@gmail.com>
2025-11-11 14:00:36 -0500

    PLEASE WORK

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// frontend/vite.config.js
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // **ADD THIS LINE**
+  base: "./",
+  // ----------------
   plugins: [react()],
-})
+});
