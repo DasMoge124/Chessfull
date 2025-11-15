535eb75975e2555893b7dd0a8376a184c8d2fb0f
DasMoge124 <kevindu0706@gmail.com>
2025-11-14 20:43:02 -0500

    YEEESSS

--- backend/chessfull/src/main/java/com/chessfull/chessfull/ChessfullApplication.java
+++ backend/chessfull/src/main/java/com/chessfull/chessfull/ChessfullApplication.java
@@ -2,12 +2,13 @@ package com.chessfull.chessfull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories; // <-- Import
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.chessfull.chessfull.UserManagement") // <-- Add this line
 public class ChessfullApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessfullApplication.class, args);
 	}
-
 }
